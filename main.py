0849e3004ce64978dfde66fadd367e0304ecd8ac
KiLeXcs <kirill.a.balan@gmail.com>
2024-10-07 10:11:31 +0300

    add borders and moving on wasd

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import pygame
-import random
+import random as rand
 
 WIDTH = 480
 HEIGHT = 480
 FPS = 60
-x, y = 230, 230
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -15,8 +14,13 @@ BLUE = (0, 0, 255)
 pygame.init()
 pygame.mixer.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("My Game")
+pygame.display.set_caption("Cars")
 clock = pygame.time.Clock()
+
+class Player():
+    x = 230
+    y = 230
+    length = 20
 
 running = True
 while running:
@@ -27,12 +31,16 @@ while running:
     screen.fill(BLACK)
 
     pressed = pygame.key.get_pressed()
-    if pressed[pygame.K_UP]: y -= 3
-    if pressed[pygame.K_DOWN]: y += 3
-    if pressed[pygame.K_LEFT]: x -= 3
-    if pressed[pygame.K_RIGHT]: x += 3
+    if (pressed[pygame.K_UP] or pressed[pygame.K_w]) and Player.y > 5: 
+        Player.y -= 3
+    if (pressed[pygame.K_DOWN] or pressed[pygame.K_s]) and Player.y < HEIGHT - Player.length - 5: 
+        Player.y += 3
+    if (pressed[pygame.K_LEFT] or pressed[pygame.K_a]) and Player.x > 5: 
+        Player.x -= 3
+    if (pressed[pygame.K_RIGHT] or pressed[pygame.K_d]) and Player.x < WIDTH - Player.length - 5: 
+        Player.x += 3
 
-    pygame.draw.rect(screen, (0, 128, 255), pygame.Rect(x, y, 20, 20))
+    pygame.draw.rect(screen, (0, 128, 255), pygame.Rect(Player.x, Player.y, Player.length, Player.length))
 
     pygame.display.flip()
     clock.tick(FPS)
