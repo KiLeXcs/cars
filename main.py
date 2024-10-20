a27974160916eae611a31ba6da1f29e3c453fe63
KiLeXcs <kirill.a.balan@gmail.com>
2024-10-20 23:37:50 +0300

    add your scores and some bug fixes & add feature

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pygame
+from cryptography.fernet import Fernet
 import time
 import os
 import random as rand
+import json
+import datetime
 
 class Colors():
     black = (0, 0, 0)
@@ -9,6 +12,9 @@ class Colors():
     red = (255, 0, 0)
     green = (0, 255, 0)
     blue = (0, 128, 255)
+    gold = (255, 215, 0)
+    silver = (192, 192, 192)
+    bronze = (205, 127, 50)
 
 #params
 WIDTH = 480
@@ -30,77 +36,69 @@ game_dir = os.path.dirname(os.path.abspath(__file__))
 font_nataliza = os.path.join(game_dir, 'nasaliza.ttf')
 font_32 = pygame.font.Font(font_nataliza, 32)
 font_64 = pygame.font.Font(font_nataliza, 64)
+key = b'es0XMVo30jLUso_mQU1PVmGh8RWnywv_arAPDENz9cU='
+#reading top score file
+if True:
+    file_name = f'{game_dir}\score.txt'
+    if not os.path.exists(file_name):
+        file = open(file_name, "w")
+        file.close()
+    file = open(file_name, "rb+")
+    content = file.read()
+    f = Fernet(key)
+
+    try:
+        top_scores = json.loads((f.decrypt(content)).decode())
+    except:
+        top_scores = {
+                    "high_scores": [{"score": 0, "date": "---"}, {"score": 0, "date": "---"}, {"score": 0, "date": "---"}, {"score": 0, "date": "---"}, {"score": 0, "date": "---"},
+                                    {"score": 0, "date": "---"}, {"score": 0, "date": "---"}, {"score": 0, "date": "---"}, {"score": 0, "date": "---"}, {"score": 0, "date": "---"}]
+                }
+        write_data = f.encrypt(json.dumps(top_scores).encode())
+        file.write(write_data)
+    file.close()
 clock = pygame.time.Clock()
 
-class Menu():
-    
-    color_play_sign = Colors.blue
-    color_best_score_text = Colors.blue
-    color_world_score_text= Colors.blue
-    color_settings_text = Colors.blue
-    color_score_text = Colors.black
-    def refresh_menu():
-        global timer
-        
-        #last crush
-        global player_crash_x, player_crash_y, player_crush_length, obstacle_crush_x, obstacle_crush_y, obstacle_crush_length
-        pygame.draw.rect(screen, Colors.blue, pygame.Rect(player_crash_x, player_crash_y, player_crush_length, player_crush_length))
-        pygame.draw.rect(screen, Colors.red, pygame.Rect(obstacle_crush_x, obstacle_crush_y, obstacle_crush_length, obstacle_crush_length))
-        
-        #some texts
-        text_menu_name = font_64.render("Star slider", True, Colors.blue)
-        text_rect_menu_name = text_menu_name.get_rect(center = (WIDTH // 2, 110))
-        screen.blit(text_menu_name, text_rect_menu_name)
-        
-        text_menu_your_score = font_32.render("Best score", True, Menu.color_best_score_text)
-        text_rect_menu_your_score = text_menu_your_score.get_rect(center = (WIDTH // 2, 400))
-        screen.blit(text_menu_your_score, text_rect_menu_your_score)
-        
-        text_menu_world_score = font_32.render("World records", True, Menu.color_world_score_text)
-        text_rect_menu_world_score = text_menu_world_score.get_rect(center = (WIDTH // 2, 450))
-        screen.blit(text_menu_world_score, text_rect_menu_world_score)
-        
-        text_menu_settings = font_32.render("Settings", True, Menu.color_settings_text)
-        text_rect_menu_settings = text_menu_settings.get_rect(center = (WIDTH // 2, 500))
-        screen.blit(text_menu_settings, text_rect_menu_settings)
-        
-        text_menu_score = font_32.render(f"Your score: {score}", True, Menu.color_score_text)
-        text_rect_menu_score = text_menu_score.get_rect(center = (WIDTH // 2, 350))
-        screen.blit(text_menu_score, text_rect_menu_score)
-        
-        if score != -1 and (time.time() - timer <= 0.4 or 0.8 <= time.time() - timer <= 1.2 or 1.6 <= time.time() - timer <= 2 or time.time() - timer >= 2.4):
-            Menu.color_score_text = Colors.blue
-        else:
-            Menu.color_score_text = Colors.black
-        
-        #draw play sign
-        pygame.draw.line(screen, Menu.color_play_sign, [(WIDTH - 100)//2, (HEIGHT - MENU)//2], [(WIDTH + 100)//2, (HEIGHT - MENU + 100)//2], 10)
-        pygame.draw.line(screen, Menu.color_play_sign, [(WIDTH + 100)//2, (HEIGHT - MENU + 100)//2], [(WIDTH - 100)//2, (HEIGHT - MENU + 200)//2], 10)
-        pygame.draw.line(screen, Menu.color_play_sign, [(WIDTH - 92)//2, (HEIGHT - MENU + 200)//2], [(WIDTH - 92)//2, (HEIGHT - MENU + 50)//2], 10)
-        
-        pygame.display.flip()
-        clock.tick(FPS)
+def clearall():
+    pygame.draw.rect(screen, Colors.black, pygame.Rect(0, 0, WIDTH, HEIGHT+MENU))
+
+def save_score(score):
+    global top_scores
+    #check is a top 10 score
+    if (top_scores["high_scores"][-1]["score"]) < score:
+        top_scores["high_scores"][-1]["score"] = score
+        top_scores["high_scores"][-1]["date"] = datetime.date.today().strftime('%Y-%m-%d')
+        top_scores["high_scores"].sort(key=lambda x: x["score"], reverse = True)
+        data_save = f.encrypt(json.dumps(top_scores).encode())
+        file = open(file_name, "wb")
+        file.write(data_save)
+        file.close()
 
 def mouse_coordinates():
     
     mouse_x, mouse_y = pygame.mouse.get_pos()
 
-    if mouse_x >= 157 and mouse_x <= 323 and mouse_y >= 390 and mouse_y <= 410:
-        Menu.color_best_score_text = Colors.white
+    if mouse_x >= 142 and mouse_x <= 337 and mouse_y >= 390 and mouse_y <= 410:
+        Menu.color_your_score_text = Colors.white
     else:
-        Menu.color_best_score_text = Colors.blue
+        Menu.color_your_score_text = Colors.blue
     
-    if mouse_x >= 119 and mouse_x <= 361 and mouse_y >= 440 and mouse_y <= 460:
+    if mouse_x >= 119 and mouse_x <= 361 and mouse_y >= 430 and mouse_y <= 450:
         Menu.color_world_score_text = Colors.white
     else:
         Menu.color_world_score_text = Colors.blue
         
-    if mouse_x >= 178 and mouse_x <= 303 and mouse_y >= 490 and mouse_y <= 510:
+    if mouse_x >= 178 and mouse_x <= 303 and mouse_y >= 470 and mouse_y <= 490:
         Menu.color_settings_text = Colors.white
     else:
         Menu.color_settings_text = Colors.blue
+
+    if mouse_x >= 210 and mouse_x <= 270 and mouse_y >= 510 and mouse_y <= 530:
+        Menu.color_exit_text = Colors.red
+    else:
+        Menu.color_exit_text = Colors.blue
     
-    if mouse_x >= 180 and mouse_x <= 300 and mouse_y >= 190 and mouse_y <= 320:
+    if mouse_x >= 180 and mouse_x <= 300 and mouse_y >= 150 and mouse_y <= 290:
         Menu.color_play_sign = Colors.white
     else:
         Menu.color_play_sign = Colors.blue
@@ -118,42 +116,104 @@ def refresh_play():
     pygame.display.flip()
     clock.tick(FPS)
 
+def startgame():
+    global playing, obstacles, score
+    playing = True
+    pygame.mouse.set_visible(False)
+    obstacles = []
+    for i in range(num_enemy):
+        obstacle = Obstacles()
+        obstacles.append(obstacle)
+    Player.x = 230
+    Player.y = 230
+    score = 0
+    refresh_play()
+    for i in range (3, 0, -1):
+        pygame.draw.rect(screen, Colors.black, pygame.Rect(WIDTH//2 - 30, HEIGHT//2 + 50, 60, 60))
+        timer_text = font_64.render(str(i), True, Colors.blue)
+        timer_rect_text = timer_text.get_rect(center = (WIDTH // 2, HEIGHT // 2 + 80))
+        screen.blit(timer_text, timer_rect_text)
+        pygame.display.flip()
+        pygame.time.wait(1000)
+
+def player_score_menu():
+    player_score_menu_open = True
+    clearall()
+    while player_score_menu_open:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:  
+                os._exit()
+            elif event.type == pygame.KEYDOWN: 
+                if event.key == pygame.K_ESCAPE:
+                    clearall()
+                    player_score_menu_open = False
+        
+        if player_score_menu_open:
+            #some text
+            text = font_32.render("Your best scores", True, Colors.blue)
+            text_rect = text.get_rect(center = (WIDTH // 2, 40))
+            screen.blit(text, text_rect)
+
+            text = font_32.render("score", True, Colors.blue)
+            text_rect = text.get_rect(center = (WIDTH // 2 - 55, 80))
+            screen.blit(text, text_rect)
+
+            text = font_32.render("date", True, Colors.blue)
+            text_rect = text.get_rect(center = (WIDTH // 2 + 130, 80))
+            screen.blit(text, text_rect)
+
+            for i in range(0, 10):
+                if i == 0:
+                    Color = Colors.gold
+                elif i == 1:
+                    Color = Colors.silver
+                elif i == 2:
+                    Color = Colors.bronze
+                else:
+                    Color = Colors.blue
+
+                #number
+                text = font_32.render(str(i+1), True, Color)
+                text_rect = text.get_rect(center = (WIDTH // 2 - 170, 80 + 40 * (i+1)))
+                screen.blit(text, text_rect)
+                
+                #score
+                text = font_32.render(str(top_scores["high_scores"][i]["score"]), True, Color)
+                text_rect = text.get_rect(center = (WIDTH // 2 - 55, 80 + 40 * (i+1)))
+                screen.blit(text, text_rect)
+
+                #date
+                text = font_32.render(str(top_scores["high_scores"][i]["date"]), True, Color)
+                text_rect = text.get_rect(center = (WIDTH // 2 + 130, 80 + 40 * (i+1)))
+                screen.blit(text, text_rect)
+
+
+            pygame.display.flip()
+
 def mouse_button():
     
     mouse_x, mouse_y = pygame.mouse.get_pos()
-    
-    #best score button
-    if mouse_x >= 157 and mouse_x <= 323 and mouse_y >= 390 and mouse_y <= 410:
-        pass
+
+    #your score button
+    if mouse_x >= 142 and mouse_x <= 337 and mouse_y >= 390 and mouse_y <= 410:
+        player_score_menu()
     
     #world record button
-    if mouse_x >= 119 and mouse_x <= 361 and mouse_y >= 440 and mouse_y <= 460:
+    if mouse_x >= 119 and mouse_x <= 361 and mouse_y >= 430 and mouse_y <= 450:
         pass
     
     #settings button
-    if mouse_x >= 178 and mouse_x <= 303 and mouse_y >= 490 and mouse_y <= 510:
+    if mouse_x >= 178 and mouse_x <= 303 and mouse_y >= 470 and mouse_y <= 490:
         pass
     
+    #exit button
+    if mouse_x >= 210 and mouse_x <= 270 and mouse_y >= 510 and mouse_y <= 530:
+        os._exit()
+
     #play button
-    if mouse_x >= 180 and mouse_x <= 300 and mouse_y >= 190 and mouse_y <= 320:
-        global playing, obstacles, score
-        playing = True
-        pygame.mouse.set_visible(False)
-        obstacles = []
-        for i in range(num_enemy):
-            obstacle = Obstacles()
-            obstacles.append(obstacle)
-        Player.x = 230
-        Player.y = 230
-        score = 0
-        refresh_play()
-        for i in range (3, 0, -1):
-            pygame.draw.rect(screen, Colors.black, pygame.Rect(WIDTH//2 - 30, HEIGHT//2 + 50, 60, 60))
-            timer_text = font_64.render(str(i), True, Colors.blue)
-            timer_rect_text = timer_text.get_rect(center = (WIDTH // 2, HEIGHT // 2 + 80))
-            screen.blit(timer_text, timer_rect_text)
-            pygame.display.flip()
-            pygame.time.wait(1000)
+    if mouse_x >= 180 and mouse_x <= 300 and mouse_y >= 150 and mouse_y <= 290:
+        startgame()
             
 class Player():
     x = 230
@@ -213,13 +273,69 @@ class Obstacles():
         if self.direction:
             self.x += self.speed
             if self.x > WIDTH or self.x < -self.length:
-                score += self.length * 10
+                score += self.length * abs(self.speed)
                 self.spawn_obstacle()
         else:
             self.y += self.speed
             if self.y > HEIGHT or self.y < -self.length:
                 score += self.length * abs(self.speed)
                 self.spawn_obstacle()
+
+class Menu():
+    
+    color_play_sign = Colors.blue
+    color_your_score_text = Colors.blue
+    color_world_score_text= Colors.blue
+    color_settings_text = Colors.blue
+    color_exit_text = Colors.blue
+    color_score_text = Colors.blue
+    
+    def refresh_menu():
+        global timer
+
+        #last crush
+        global player_crash_x, player_crash_y, player_crush_length, obstacle_crush_x, obstacle_crush_y, obstacle_crush_length
+
+        if score != -1 and (time.time() - timer <= 0.4 or 0.8 <= time.time() - timer <= 1.2 or 1.6 <= time.time() - timer <= 2 or time.time() - timer >= 2.4):
+            pygame.draw.rect(screen, Colors.blue, pygame.Rect(player_crash_x, player_crash_y, player_crush_length, player_crush_length))
+            pygame.draw.rect(screen, Colors.red, pygame.Rect(obstacle_crush_x, obstacle_crush_y, obstacle_crush_length, obstacle_crush_length))
+            text_menu_score = font_32.render(f"Your score: {score}", True, Menu.color_score_text)
+            text_rect_menu_score = text_menu_score.get_rect(center = (WIDTH // 2, 330))
+            screen.blit(text_menu_score, text_rect_menu_score)
+        else:
+            pygame.draw.rect(screen, Colors.black, pygame.Rect(0, 305, WIDTH, 50))
+            pygame.draw.rect(screen, Colors.blue, pygame.Rect(player_crash_x, player_crash_y, player_crush_length, player_crush_length))
+            pygame.draw.rect(screen, Colors.red, pygame.Rect(obstacle_crush_x, obstacle_crush_y, obstacle_crush_length, obstacle_crush_length))
+
+        
+        #some texts
+        text_menu_name = font_64.render("Star slider", True, Colors.blue)
+        text_rect_menu_name = text_menu_name.get_rect(center = (WIDTH // 2, 80))
+        screen.blit(text_menu_name, text_rect_menu_name)
+        
+        text_menu_your_score = font_32.render("Your scores", True, Menu.color_your_score_text)
+        text_rect_menu_your_score = text_menu_your_score.get_rect(center = (WIDTH // 2, 400))
+        screen.blit(text_menu_your_score, text_rect_menu_your_score)
+        
+        text_menu_world_score = font_32.render("World records", True, Menu.color_world_score_text)
+        text_rect_menu_world_score = text_menu_world_score.get_rect(center = (WIDTH // 2, 440))
+        screen.blit(text_menu_world_score, text_rect_menu_world_score)
+        
+        text_menu_settings = font_32.render("Settings", True, Menu.color_settings_text)
+        text_rect_menu_settings = text_menu_settings.get_rect(center = (WIDTH // 2, 480))
+        screen.blit(text_menu_settings, text_rect_menu_settings)
+
+        text_menu_settings = font_32.render("Exit", True, Menu.color_exit_text)
+        text_rect_menu_settings = text_menu_settings.get_rect(center = (WIDTH // 2, 520))
+        screen.blit(text_menu_settings, text_rect_menu_settings)
+        
+        #draw play sign
+        pygame.draw.line(screen, Menu.color_play_sign, [(WIDTH - 100)//2, (HEIGHT - MENU - 60)//2], [(WIDTH + 100)//2, (HEIGHT - MENU + 40)//2], 10)
+        pygame.draw.line(screen, Menu.color_play_sign, [(WIDTH + 100)//2, (HEIGHT - MENU + 40)//2], [(WIDTH - 100)//2, (HEIGHT - MENU + 140)//2], 10)
+        pygame.draw.line(screen, Menu.color_play_sign, [(WIDTH - 92)//2, (HEIGHT - MENU + 140)//2], [(WIDTH - 92)//2, (HEIGHT - MENU - 10)//2], 10)
+        
+        pygame.display.flip()
+        clock.tick(FPS)
 
 while running:
     #quit
@@ -229,7 +345,9 @@ while running:
         #check mouse click
         if event.type == pygame.MOUSEBUTTONUP:
             mouse_button()
-    
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN:
+                startgame()
     #get coordinate of mouse to change colors of texts
     mouse_coordinates()
     
@@ -250,8 +368,10 @@ while running:
                 playing = False
                 player_crash_x, player_crash_y, player_crush_length = Player.x, Player.y, Player.length
                 obstacle_crush_x, obstacle_crush_y, obstacle_crush_length = obstacle.x, obstacle.y, obstacle.length
+                #cleaning screen
                 pygame.draw.rect(screen, Colors.black, pygame.Rect(0, 0, WIDTH, HEIGHT + MENU))
                 pygame.mouse.set_visible(True)
+                save_score(score)
                 timer = time.time()
         
         if playing:
@@ -260,5 +380,3 @@ while running:
 
             #draw
             refresh_play()
-
-pygame.quit()
